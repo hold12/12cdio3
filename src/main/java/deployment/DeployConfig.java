a957c620fd9b053273340c5ec660c2b59b17b263
Anders W. Olsen <anders@wiberg.tech>
2017-05-02 02:22:31 +0200

    changing cn redirect

--- src/main/java/deployment/DeployConfig.java
+++ src/main/java/deployment/DeployConfig.java
@@ -5,7 +5,7 @@ package deployment;
  */
 public class DeployConfig {
     private static final String CAMPUSNET_REDIRECT_URL = "CAMPUSNET_REDIRECT_URL";
-    private static final String DEFAULT_CN_REDIRECT_URL = "http://localhost:8080/rest/cn";
+    private static final String DEFAULT_CN_REDIRECT_URL = "https://f1712cdio3.herokuapp.com/rest/cn";
     public static final String CN_REDIRECT_URL = (System.getenv(CAMPUSNET_REDIRECT_URL) != null) ?
             System.getenv(CAMPUSNET_REDIRECT_URL): DEFAULT_CN_REDIRECT_URL;
 }
