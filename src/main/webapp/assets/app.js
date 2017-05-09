25de0a0427ce249f52f06c648056ed2c01c7affc
Anders W. Olsen <anders@wiberg.tech>
2017-05-09 11:19:38 +0200

    Showing users in a table

---
+++ src/main/webapp/assets/app.js
@@ -0,0 +1,22 @@
+/**
+ * Created by AndersWOlsen on 09-05-2017.
+ */
+$(function() {
+    showUsers("rest/users");
+});
+
+function showUsers(url) {
+    $.getJSON(url , function(data) {
+        var tbl_body = "";
+        var odd_even = false;
+        $.each(data, function() {
+            var tbl_row = "";
+            $.each(this, function(k , v) {
+                tbl_row += "<td>"+v+"</td>";
+            })
+            tbl_body += "<tr class=\""+( odd_even ? "odd" : "even")+"\">"+tbl_row+"</tr>";
+            odd_even = !odd_even;
+        })
+        $("#users-table").append(tbl_body);
+    });
+}
