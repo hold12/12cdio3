e6b62351eca42e128a8950e996dae957e66071a1
Freya Helstrup <freyagh@me.com>
2017-05-09 13:18:05 +0200

    Added frontend stuff

--- src/main/webapp/assets/app.js
+++ src/main/webapp/assets/app.js
@@ -14,6 +14,7 @@ function showUsers(url) {
             $.each(this, function(k , v) {
                 tbl_row += "<td>"+v+"</td>";
             })
+            tbl_row += "<a id=\"delete-user-btn\" class=\"button\" onclick=\"deleteUser(this)\">Delete user</a>";
             tbl_body += "<tr class=\""+( odd_even ? "odd" : "even")+"\">"+tbl_row+"</tr>";
             odd_even = !odd_even;
         })
@@ -21,9 +22,17 @@ function showUsers(url) {
     });
 }
 
+function deleteUser(btn) {
+    var row = btn.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
 
 $("#create-user-btn").click(function() {
     $("#create-user-btn").css("display", "none");
     $("#show_users").css("display", "none");
     $("#create_user").css("display", "block");
+});
+
+$("#delete-user-btn").click(function() {
+    deleteUser(btn);
 });
