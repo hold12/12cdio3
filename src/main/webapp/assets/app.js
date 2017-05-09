d5faf50c661ede00bfa8e6ae12f4fb538922dc07
Anders W. Olsen <anders@wiberg.tech>
2017-05-09 11:14:31 +0200

    Can now retrieve users

--- src/main/webapp/assets/app.js
+++ src/main/webapp/assets/app.js
@@ -1,40 +1,19 @@
 $(function() {
-    var showData = $('#testing');
-
-    $.getJSON('rest/users', function (data) {
-        console.log(data);
-
-        var items = data.items.map(function (item) {
-            return item.key + ': ' + item.value;
-        });
-
-        showData.empty();
-
-        if (items.length) {
-            var content = '<li>' + items.join('</li><li>') + '</li>';
-            var list = $('<ul />').html(content);
-            showData.append(list);
-        }
-    });
-
-    showData.text('Loading the JSON file.');
+    showUsers("rest/users");
 });
 
-function CallURL() {
-    $.ajax({
-        url: 'rest/users',
-        type: "GET",
-        dataType: "jsonp",
-        async: false,
-        success: function(data) {
-            ShowData(data);
-        },
-        error: function() {
-            $("#testing").text("Error...");
-        }
+function showUsers(url) {
+    $.getJSON(url , function(data) {
+        var tbl_body = "";
+        var odd_even = false;
+        $.each(data, function() {
+            var tbl_row = "";
+            $.each(this, function(k , v) {
+                tbl_row += "<td>"+v+"</td>";
+            })
+            tbl_body += "<tr class=\""+( odd_even ? "odd" : "even")+"\">"+tbl_row+"</tr>";
+            odd_even = !odd_even;
+        })
+        $("#users-table").append(tbl_body);
     });
-}
-
-function ShowData(json) {
-    $("#testing").text(json.result);
 }
